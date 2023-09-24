2d866de107967bdbaa252013601ec8d2cc98d399
Anshuman Solanki <anshumansolanki@Anshumans-MacBook-Air.local>
2023-09-24 18:54:01 +0530

    chore: try using components as parameter in table

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
 import './App.css';
 import CustomTable from './CustomTable.tsx';
 
+function labelTag (value) {
+    return (
+      <h1>
+        {value}
+      </h1>
+    )
+} 
+
 function App() {
   const inputData = [
-    ['Name', 'Birth', 'Place'],
+    [labelTag('firdtName'), labelTag('lastName'), labelTag('year of birth')],
     ['Anshuman','Solanki', 1998],
     ['Somesh', 'Solanki', 2002]
   ]
   const columnConfig = {
-      0 : {
-        width: '200px',
-        textColor : 'white',
-        backgroundColor: 'black',
-      },    
-      1 : { 
-        width : '100px',
-      },
-      2 : {
-        width: '100px',
-        textColor : 'blue',
-      }
+    0 : {
+      width: '200px',
+    },    
+    1 : { 
+      width : '200px',
+    },
+    2 : {
+      width: '200px',
     }
+  }
 
-    const rowConfig = {
-      0: {
-        borderBottom: '2px solid white',
-        fontWeight: '700'
-      },
-      1 : {
-        borderBottom: '1px solid white'
-      }
+  const rowConfig = {
+    0: {
+      borderBottom: '2px solid black',
+      fontWeight: '700'
+    },
+    1 : {
+      borderBottom: '1px solid white'
     }
+  }
 
   return (
     <div className="App">
